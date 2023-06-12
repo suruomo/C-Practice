eeb039cadb28123ec61f514e4839810a6da1ae10
suruomo <www.suruomo@hotmail.com>
2023-06-12 20:56:12 +0800

    回文数

---
+++ leetcode/IsPalindrome.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x);
+
+int main()
+{
+    int x = 121;
+    printf("%d", isPalindrome(x));
+    return 0;
+}
+/*
+* 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+*/
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+    else
+    {
+        long int a, b = 0;
+        a = x;
+        while (a != 0)
+        {
+            b = b * 10 + a % 10;
+            a = a / 10;
+        }
+        if (x == b)
+            return true;
+        else
+            return false;
+
+    }
+
+}
--- leetcode/TwoSum.c
+++ leetcode/TwoSum.c
@@ -3,17 +3,21 @@
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize);
 
-int main()
-{
-    int target = 9;
-    int nums[4] = { 2,7,11,15 };
-    int size = 2;
-    int* result = NULL;
-    result=twoSum(nums, 2, target, &size);
-    printf(" % d, % d", result[0], result[1]);
-	return 0;
-}
+//int main()
+//{
+//    int target = 9;
+//    int nums[4] = { 2,7,11,15 };
+//    int size = 2;
+//    int* result = NULL;
+//    result=twoSum(nums, 2, target, &size);
+//    printf(" % d, % d", result[0], result[1]);
+//	return 0;
+//}
 
+/*
+* 给定一个整数数组 nums 和一个整数目标值 target，
+请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+*/
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int i, j;
     int* result = (int*)malloc(sizeof(int) * 2);
